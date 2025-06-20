5a4e0b23b7e88a3ea3c54fa8a28332c9b66b2427
Dinos <konstadinos.as@gmail.com>
2025-06-20 16:49:42 +0300

    db added

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -73,11 +73,18 @@ WSGI_APPLICATION = 'LittleLemon.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES = {  
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'capstone',  
+        'USER': 'myuser',  
+        'PASSWORD': 'mypassword',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
@@ -118,7 +125,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    BASE_DIR / "restaurant" / "static",
 ]
 
 # Default primary key field type
